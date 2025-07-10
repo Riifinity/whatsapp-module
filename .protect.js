58afc98dad425b51e241463ff7b7399fb77b0f60
Riifinity <ngapainn71@gmail.com>
2025-07-10 23:53:39 +0800

    Update .protect.js

--- .protect.js
+++ .protect.js
@@ -8,40 +8,40 @@ try {
 
     const infoFile = path.join(__dirname, 'InfoRiifinity.js');
     if (!fs.existsSync(infoFile)) {
-        console.log(`\n${RED_BOLD}Riifinity Security Detected\n`);
-        console.error(`\n${RED_BOLD}🚫 File InfoRiifinity.js tidak ditemukan.${RESET}`);
+        console.log(`\n${RED_BOLD}Riifinity Security Detected :\n${RESET}`);
+        console.error(`${RED_BOLD}🚫 File InfoRiifinity.js tidak ditemukan.${RESET}`);
         process.exit(1);
     }
 
     const mainScriptPath = path.join(process.cwd(), 'main.js');
     if (!fs.existsSync(mainScriptPath)) {
-        console.log(`\n${RED_BOLD}Riifinity Security Detected\n`);
-        console.error(`\n${RED_BOLD}🚫 File main.js tidak ditemukan di root project.${RESET}`);
+        console.log(`\n${RED_BOLD}Riifinity Security Detected :\n${RESET}`);
+        console.error(`${RED_BOLD}🚫 File main.js tidak ditemukan di root project.${RESET}`);
         process.exit(1);
     }
 
     const mainScript = fs.readFileSync(mainScriptPath, 'utf-8');
 
     if (!mainScript.includes('await infoRiifinity')) {
-        console.log(`\n${RED_BOLD}Riifinity Security Detected\n`);
-        console.error(`\n${RED_BOLD}🚫 Creadit Riifinity Telah Dihapus\n`);
-        console.error(`${RED_BOLD}Jangan Hapus Creadit!!!\n`);
-        console.error(`${RED_BOLD}Kembalikan Seperti Semula Agar Tidak Error!!!${RESET}\n\n`);
+        console.log(`\n${RED_BOLD}Riifinity Security Detected :\n${RESET}`);
+        console.error(`${RED_BOLD}🚫 Creadit Riifinity Telah Dihapus${RESET}`);
+        console.error(`${RED_BOLD}Jangan Hapus Creadit!!!${RESET}`);
+        console.error(`${RED_BOLD}Kembalikan Seperti Semula Agar Tidak Error!!!${RESET}`);
         process.exit(1);
     }
 
     if (!mainScript.includes("require('./node_modules/riifinity/InfoRiifinity.js')")) {
-        console.log(`\n${RED_BOLD}Riifinity Security Detected\n`);
-        console.error(`\n${RED_BOLD}🚫 Creadit Riifinity Telah Dihapus\n`);
-        console.error(`${RED_BOLD}Jangan Hapus Creadit!!!\n`);
-        console.error(`${RED_BOLD}Kembalikan Seperti Semula Agar Tidak Error!!!${RESET}\n\n`);
+        console.log(`\n${RED_BOLD}Riifinity Security Detected :\n${RESET}`);
+        console.error(`${RED_BOLD}🚫 Creadit Riifinity Telah Dihapus${RESET}`);
+        console.error(`${RED_BOLD}Jangan Hapus Creadit!!!${RESET}`);
+        console.error(`${RED_BOLD}Kembalikan Seperti Semula Agar Tidak Error!!!${RESET}`);
         process.exit(1);
     }
 
 } catch (err) {
     const RED_BOLD = '\x1b[1m\x1b[31m';
     const RESET = '\x1b[0m';
-    console.log('Riifinity Security Detected\n');
+    console.log(`\n${RED_BOLD}Riifinity Security Detected :\n${RESET}`);
     console.error(`${RED_BOLD}❌ Riifinity Security Blocked:${RESET}`);
     console.error(`${RED_BOLD}${err.message}${RESET}`);
     process.exit(1);

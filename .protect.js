16fb08f29a39ad5e02199d7290b0b7f6fc5cd801
Riifinity <ngapainn71@gmail.com>
2025-07-10 23:08:21 +0800

    Update .protect.js

--- .protect.js
+++ .protect.js
@@ -6,24 +6,24 @@ try {
     // Cek apakah InfoRiifinity.js tersedia
     const infoFile = path.join(__dirname, 'InfoRiifinity.js');
     if (!fs.existsSync(infoFile)) {
-        throw new Error("ðŸš« File InfoRiifinity.js tidak ditemukan.");
+        throw new Error("File InfoRiifinity.js tidak ditemukan.");
     }
 
     // Cek isi file utama index.js
     const mainScriptPath = path.join(process.cwd(), 'main.js'); // Ganti sesuai nama utama kamu jika beda
     if (!fs.existsSync(mainScriptPath)) {
-        throw new Error("ðŸš« File main.js tidak ditemukan di root project.");
+        throw new Error("File main.js tidak ditemukan di root project.");
     }
 
     const mainScript = fs.readFileSync(mainScriptPath, 'utf-8');
 
     // Validasi pemanggilan fungsi dan require
-    if (!mainScript.includes('await infoRiifinity(sock)')) {
-        throw new Error("ðŸš« Creadit Riifinity Telah Dihapus ( JANGAN HAPUS CREADIT !!).");
+    if (!mainScript.includes('await infoRiifinity')) {
+        throw new Error("Creadit Riifinity Telah Dihapus ( JANGAN HAPUS CREADIT !!).");
     }
 
     if (!mainScript.includes("require('./node_modules/riifinity/InfoRiifinity.js')")) {
-        throw new Error("ðŸš« Creadit Riifinity Telah Dihapus ( JANGAN HAPUS CREADIT !!).");
+        throw new Error("Creadit Riifinity Telah Dihapus ( JANGAN HAPUS CREADIT !!).");
     }
 
 } catch (err) {
